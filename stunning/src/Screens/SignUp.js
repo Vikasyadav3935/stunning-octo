6cd93f3060fb2339bc3f6ffabdcdcbf805a63a78
Vikas Yadav <vikasyadav3935@gmail.com>
2023-02-16 01:22:15 +0530

    fire

--- stunning/src/Screens/SignUp.js
+++ stunning/src/Screens/SignUp.js
@@ -1,13 +1,41 @@
-import {View, Text,TextInput,TouchableOpacity} from 'react-native';
-import React from 'react';
+import {View, Text,TextInput,TouchableOpacity,alert,Alert} from 'react-native';
+import React, {useState} from 'react';
+import firestore from '@react-native-firebase/firestore';
+import { v4 as uuidv4, v4 } from 'uuid';
 
 const SignUp = ({navigation}) => {
+
+ const [email,setEmail]=useState('');
+ const [password,setPassword]=useState('');
+ const [name,setName]=useState('')
+  
+ const signUp=()=>{
+  const userId=Math.floor((Math.random() * 100) + 1);
+  firestore()
+  .collection('Users')
+  .doc(userId)
+  .set({
+    name: name,
+    email:email,
+    password:password,
+    userId:userId
+  })
+  .then(() => {
+    console.log('User added!');
+  });
+
+
+ }
+
+
   return (
     <View>
-      <Text style={{fontSize: 20, alignSelf: 'center', marginTop: 100}}>
+      <Text style={{fontSize: 20, alignSelf: 'center', marginTop: 60}}>
         SignUp
       </Text>
       <TextInput
+       onChangeText={setName}
+       value={name}
         placeholder="Name"
         style={{
           width: '90%',
@@ -20,6 +48,8 @@ const SignUp = ({navigation}) => {
         }}
       />
       <TextInput
+      onChangeText={setEmail}
+      value={email}
         placeholder="Email"
         style={{
           width: '90%',
@@ -34,6 +64,8 @@ const SignUp = ({navigation}) => {
 
       <TextInput
         placeholder="Enter Password"
+        onChangeText={setPassword}
+       value={password}
         secureTextEntry={true}
         style={{
           width: '90%',
@@ -56,7 +88,16 @@ const SignUp = ({navigation}) => {
           alignItems: 'center',
           justifyContent: 'center',
           borderRadius: 10,
-        }}>
+        }}
+        onPress={()=>{
+          if(name!=='' && password !=='' && email !=''){
+            signUp();
+          }else{
+            Alert.alert('Fill all details');
+          }
+        }}
+        
+        >
         <Text style={{fontSize: 20, color: 'wheat'}}>Login</Text>
       </TouchableOpacity>
       <Text
